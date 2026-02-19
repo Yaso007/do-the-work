bef26f9e68e0fd7bfea5bf121288661231487163
Yaso007 <yashopmukhiazz007@gmail.com>
2026-02-19 11:42:24 +0530

    todo.routes complete

--- backend/src/routes/todo.routes.js
+++ backend/src/routes/todo.routes.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import authMiddleware from  '../middlewares/auth.middleware.js';
+import * as todoController from '../controllers/todo.controller.js';
+
+
 
 const router = express.Router();
 
@@ -8,5 +11,10 @@ router.use(authMiddleware);
 router.get('/',(req,res) => {
     res.send("hello world")
 })
+
+router.post('/', todoController.createTodo);
+router.get('/', todoController.getTodos);
+router.put('/',todoController.updateTodo);
+router.delete('/', todoController.deleteTodo)
 
 export default router;
