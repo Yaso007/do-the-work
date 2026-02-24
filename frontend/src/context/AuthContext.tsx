f91ef858dfd2abbde570d803f0fc90f714e31dbf
Yaso007 <yashopmukhiazz007@gmail.com>
2026-02-24 13:43:03 +0530

    feat:authentication context

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import type {User} from "../types/auth";
+
+interface AuthContextType {
+    user: User |null;
+    token: string |null;
+    login: (token:string,user:User) => void;
+    logout: ()=>void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider = ({children}:{children:ReactNode}) =>{
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null> (null);
+    
+    const login = (token:string,user:User) =>{
+        localStorage.setItem("token",token);
+        setToken(token);
+        setUser(user);
+
+    };
+    const logout = () => {
+        localStorage.removeItem("token");
+        setToken(null);
+        setUser(null);
+
+    };
+
+    return (
+        <AuthContext.Provider value = {
+            {user, token, login, logout}
+        }>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("useAuth must be used within an AuthProvider");
+    }
+    return context;
+};
+
